0f5a300e4dd035c519fd3a97e4a7b4d240539779
gerasy1987 <gs2716@columbia.edu>
2021-06-07 22:56:50 +0200

    nested group prevalence and size in estimands (for within RDS prevalence estimation)

--- R/study_estimands.R
+++ R/study_estimands.R
@@ -14,7 +14,11 @@ get_study_estimands <- function(data,
                                 known_pattern = "^known(\\_\\d|\\d)?$",
                                 hidden_var = "hidden") {
 
-  data %>%
+
+  known_groups <- names(data)[grepl(x = names(data), pattern = known_pattern)]
+
+  .out <-
+    data %>%
     # dplyr::mutate(
     #   degree_all = purrr::map_int(links, ~ length(strsplit(.x, split = ";")[[1]])),
     #   degree_hidden =
@@ -32,13 +36,35 @@ get_study_estimands <- function(data,
                     list(prev = mean), na.rm  = TRUE),
       # dplyr::across(c(degree_all, degree_hidden), mean, na.rm  = TRUE)
       ) %>%
-
     {
       data.frame(
         inquiry_label = names(.),
         estimand = unname(t(.)),
         stringsAsFactors = FALSE)
-    } %>%
-    {.[!grepl(pattern = known_pattern, .$inquiry_label),]}
+    }# %>%
+    # {.[!grepl(pattern = known_pattern, .$inquiry_label),]}
 
+  if (length(known_groups) != 0) {
+    for (i in seq_along(known_groups)) {
+      .out <-
+        data %>%
+        dplyr::filter(eval(parse(text = paste0(known_groups[i], " == 1")))) %>%
+        dplyr::summarise(
+          dplyr::across(c(dplyr::matches(paste0("^", hidden_var, "$"))),
+                        list(size = sum), na.rm  = TRUE),
+          dplyr::across(c(dplyr::matches(paste0("^", hidden_var, "$"))),
+                        list(prev = mean), na.rm  = TRUE),
+          # dplyr::across(c(degree_all, degree_hidden), mean, na.rm  = TRUE)
+        ) %>%
+        {
+          data.frame(
+            inquiry_label = paste0(names(.), "_in_", known_groups[i]),
+            estimand = unname(t(.)),
+            stringsAsFactors = FALSE)
+        } %>%
+        bind_rows(.out, .)
+    }
+  }
+
+  return(.out)
 }
