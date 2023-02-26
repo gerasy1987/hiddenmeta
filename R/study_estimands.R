2100ef85144d21073a23c63c30756a3b1d4fe292
gerasy1987 <gs2716@columbia.edu>
2023-02-26 19:34:06 +0100

    add in and out group prevalence for study estmands

--- R/study_estimands.R
+++ R/study_estimands.R
@@ -73,7 +73,8 @@ get_study_estimands_tidy <- function(data,
 #' Get individual study estimands using data.table
 #'
 #' @param data pass-through population data frame
-#' @param known_pattern character string containing regular expression to match known group names in the study population dataset
+#' @param in_groups character string containing regular expression to match group names for which prevalence is estimated within hidden group
+#' @param out_groups character string containing regular expression to match group names within which hidden group prevalence is estimated
 #' @param hidden_var character string containing hidden group name in the study population data frame
 #'
 #' @return Estimands data frame for single study
@@ -81,16 +82,21 @@ get_study_estimands_tidy <- function(data,
 #'
 #' @import data.table
 get_study_estimands <- function(data,
-                                   known_pattern = "^known(\\_\\d|\\d)?$",
-                                   hidden_var = "hidden") {
+                                in_groups = NULL,
+                                out_groups = "^known(\\_\\d|\\d)?$",
+                                hidden_var = "hidden") {
 
   if (!data.table::is.data.table(data)) data <- data.table::setDT(data)
 
-  known_groups <-
-    names(data)[grepl(x = names(data), pattern = known_pattern)]
+  .out_groups_vec <-
+    names(data)[grepl(x = names(data), pattern = out_groups)]
+
+  .in_groups_vec <-
+    names(data)[grepl(x = names(data), pattern = in_groups)]
+
 
   .out <-
-    data[, grep(pattern = paste0(known_pattern, "|^", hidden_var, "$"),
+    data[, grep(pattern = paste0(in_groups, "|", out_groups, "|^", hidden_var, "$"),
                        x = names(data), value = TRUE)
        , with = FALSE]
 
@@ -101,14 +107,30 @@ get_study_estimands <- function(data,
         X = names(.out),
         FUN = function(x)
           data.frame(inquiry = paste0(x, c("_size", "_prev")),
-                     estimand = c(sum(!is.na(.out[[x]])),
+                     estimand = c(sum(.out[[x]], na.rm = TRUE),
                                   mean(.out[[x]], na.rm = TRUE)))))
 
-  if (length(known_groups) != 0) {
-    for (i in seq_along(known_groups)) {
+  # get prevalences and sizes of other groups within hidden group
+  if (!is.null(.in_groups_vec)) {
+    for (i in seq_along(.in_groups_vec)) {
+      .temp <-
+        data[get(hidden_var) == 1, .in_groups_vec[i], with = FALSE]
+
+      .out <-
+        rbind(.out,
+              data.frame(
+                inquiry = paste0(names(.temp), c("_size", "_prev"), "_in_", hidden_var),
+                estimand = c(sum(unlist(.temp), na.rm = TRUE),
+                             mean(unlist(.temp), na.rm = TRUE))))
+    }
+  }
+
+  # get prevalences and sizes of hidden group within other groups
+  if (!is.null(.out_groups_vec)) {
+    for (i in seq_along(.out_groups_vec)) {
       .temp <-
         data[
-          get(known_groups[i]) == 1
+          get(.out_groups_vec[i]) == 1
           , grep(pattern = paste0("^", hidden_var, "$"), names(data), value = TRUE)
           , with = FALSE
         ]
@@ -116,8 +138,8 @@ get_study_estimands <- function(data,
       .out <-
         rbind(.out,
               data.frame(
-                inquiry = paste0(names(.temp), c("_size", "_prev"), "_in_", known_groups[i]),
-                estimand = c(sum(!is.na(unlist(.temp))),
+                inquiry = paste0(names(.temp), c("_size", "_prev"), "_in_", .out_groups_vec[i]),
+                estimand = c(sum(unlist(.temp), na.rm = TRUE),
                              mean(unlist(.temp), na.rm = TRUE))))
     }
   }
