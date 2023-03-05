278cec8e95bd109174b2220ae4cfeb15836b1a6b
gerasy1987 <gs2716@columbia.edu>
2023-03-05 22:30:59 +0100

    fix rcpp export issue by stating globals

--- R/globals.R
+++ R/globals.R
@@ -1,8 +1,12 @@
-globalVariables(
-  c(".", "study", "estimand_label", "links", "degree", "degree_hidden",
-    "N", "K", "name", "p_visibility_known", "p_visibility_hidden", "hidden",
-    "showup", "to", "from", "wave", "formalArgs", ":=", "loc_sampled",
-    "prop_sample", "rds", "sd", "prior_median", "prop_prob", "prop_share",
-    "fit_ht", "own_coupon", "pattern", "total", "population",
-    "estimands", "estimand", "estimator", "estimates", "estimate", "se",
-    "estimator"))
+# globalVariables(
+#   c(".", "study", "estimand_label", "links", "degree", "degree_hidden",
+#     "N", "K", "name", "p_visibility_known", "p_visibility_hidden", "hidden",
+#     "showup", "to", "from", "wave", "formalArgs", ":=", "loc_sampled",
+#     "prop_sample", "rds", "sd", "prior_median", "prop_prob", "prop_share",
+#     "fit_ht", "own_coupon", "pattern", "total", "population",
+#     "estimands", "estimand", "estimator", "estimates", "estimate", "se",
+#     "estimator"))
+
+#' @useDynLib hiddenmeta
+#' @importFrom Rcpp sourceCpp
+NULL
