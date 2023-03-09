d0efbd3b9ea328012829545388b7b9ffa4d40805
gerasy1987 <gs2716@columbia.edu>
2023-03-10 00:37:10 +0100

    add global variables to avoid warnings

--- R/globals.R
+++ R/globals.R
@@ -1,11 +1,18 @@
-# globalVariables(
-#   c(".", "study", "estimand_label", "links", "degree", "degree_hidden",
-#     "N", "K", "name", "p_visibility_known", "p_visibility_hidden", "hidden",
-#     "showup", "to", "from", "wave", "formalArgs", ":=", "loc_sampled",
-#     "prop_sample", "rds", "sd", "prior_median", "prop_prob", "prop_share",
-#     "fit_ht", "own_coupon", "pattern", "total", "population",
-#     "estimands", "estimand", "estimator", "estimates", "estimate", "se",
-#     "estimator"))
+globalVariables(
+  c(".", "..cols", "..hidden_var", "..sampling_frame", "Label", "N",
+    "Variable", "_from", "as.formula", "cluster_id", "cluster_prop", "dat",
+    "description", "empty", "estimand", "estimands", "estimate", "estimator",
+    "estimator_full", "example", "family", "frame", "from", "group",
+    "hidden_visible_out", "inquiry", "label", "links", "links_list", "loc",
+    "loc_sampled", "loc_sampling_prob", "locs_sampled", "mean_estimand",
+    "median", "n_visible_out", "na.omit", "name", "own_coupon", "params",
+    "population", "rbinom", "rexp", "rel_bias", "rescale_wgt", "rmse", "samp_est",
+    "sampled", "sampled_cluster", "sd", "se", "selected_estimator",
+    "selected_estimators", "selected_sample", "selected_samples",
+    "selected_sim", "showup", "sim_ID", "sim_type", "strata_id",
+    "strata_prop", "study", "study_id", "target", "target_n", "temp_id",
+    "terms", "to", "total", "type", "type_visible", "unit_sampling_prob",
+    "variable", "wave", "weight", "weight.scale"))
 
 #' @useDynLib hiddenmeta
 #' @importFrom Rcpp sourceCpp
